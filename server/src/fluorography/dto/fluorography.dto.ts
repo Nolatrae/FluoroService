2b8dad7a5736b5d9d71069f88a68dbd3e1ecdedc
Егор Ежов <105476991+Nolatrae@users.noreply.github.com>
2024-04-21 22:17:40 +0800

    feat(prisma): change model User

--- server/src/fluorography/dto/fluorography.dto.ts
+++ server/src/fluorography/dto/fluorography.dto.ts
@@ -1,16 +1,9 @@
-import { IsDate, IsString } from 'class-validator'
+import { IsString } from 'class-validator'
 
 export class FluorographyDto {
-	// @IsString()
-	// @IsOptional()
-	// id: string
-
 	@IsString()
 	fileUrl: string
 
-	@IsDate()
-	date: Date
-
-	// @IsOptional()
-	// userId: string
+	@IsString()
+	date: string
 }
