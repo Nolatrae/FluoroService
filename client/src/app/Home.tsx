a5af25a8d5b8d4206aa1eb9496c06100422ddae2
Егор Ежов <105476991+Nolatrae@users.noreply.github.com>
2024-05-06 22:56:14 +0800

    fix

--- client/src/app/Home.tsx
+++ client/src/app/Home.tsx
@@ -4,26 +4,5 @@ import { useRouter } from 'next/navigation'
 
 export function Home() {
 	const { push } = useRouter()
-
-	// const { user } = useProfile()
-
-	// const { mutate: mutateLogout, isPending: isLogoutPending } = useMutation({
-	// 	mutationKey: ['logout'],
-	// 	mutationFn: () => authService.logout(),
-	// 	onSuccess() {
-	// 		push(PUBLIC_PAGES.LOGIN)
-	// 	},
-	// })
-
-	return (
-		<>
-			{/* {' '}
-			{user && (
-				<div>
-					<h2 className='text-2xl font-bold'>Привет, {user.firstName}</h2>
-					<p className='text-lg'>Ваш email: {user.email}</p>
-				</div>
-			)} */}
-		</>
-	)
+	return <>56456</>
 }
